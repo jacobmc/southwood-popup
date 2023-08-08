1d78a77a83abda7de4eacdee70cd24724fe2e18f
Jacob McKinney <jacobmcdev@gmail.com>
2023-08-07 22:31:36 -0500

    Fixed timing logic

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
     const now = new Date();
 
     // Sunday morning from 9:25am until 11:00am
-    if (now.getUTCDay() === 0 && ((now.getUTCHours() === 14 && now.getUTCMinutes() >= 25) || (now.getUTCHours() >= 14 && now.getUTCHours() <= 16))) {
+    if (now.getUTCDay() === 0 && ((now.getUTCHours() === 14 && now.getUTCMinutes() >= 25) || (now.getUTCHours() >= 15 && now.getUTCHours() < 16))) {
         const body = document.body,
             sundayPopupClose = document.getElementById('sunday-popup-close');
     
